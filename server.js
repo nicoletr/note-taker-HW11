94d93d11758c4eb9be2d4109fe1e8fb6cf002baf
Nicoletr <nicole.trpevski@gmail.com>
2021-11-06 11:20:30 +0800

    Fixed bugs in routes & added gitignore

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path")
-const notesData = require("./Develop/db/db.json")
+const notesData = require("./db/db.json")
 
 const app = express();
 const PORT = 3001;
 
+//Middleware to set up express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+//Route to return the notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+//Route to fetch the notes data
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+//Wildard route which returns the index.html 
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
-});
-
-app.get('/api/notes', (req, res) => res.json(notesData));
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
