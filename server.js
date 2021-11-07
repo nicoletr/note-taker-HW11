ef5a0e419fb8276a5d92435b00c7ae297495edaf
Nicoletr <nicole.trpevski@gmail.com>
2021-11-07 16:29:19 +0800

    port was not uppercase. fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path")
 const api = require('./routes/index');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 //Middleware to set up express app
 app.use(express.urlencoded({ extended: true }));
